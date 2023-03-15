6daa0eef2422909ed16fd688ef8392cce4900092
adenhack <mesut.kilinc.12@gmail.com>
2023-03-15 11:10:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { Button } from "reactstrap";
 
 function App() {
+  const [sayi,setsayi] = useState(0);
+
+  const increase = () =>{
+    setsayi(sayi + 1);
+  }
+
+  const decrease = () =>{
+    setsayi(sayi - 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>currently value: {sayi}</h1>
+      
+      <Button onClick={increase}>increase</Button>
+      <Button onClick={decrease}>decrease</Button>
+
     </div>
   );
 }
